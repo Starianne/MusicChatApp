1b77855e01c212e1c2d16e9816c2ebb9579c3b63
Annelies <118388665+Starianne@users.noreply.github.com>
2026-02-04 18:52:46 +0000

    little chat echoes back but not to other pages yet

--- spotify_chat_app_project/asgi.py
+++ spotify_chat_app_project/asgi.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+
 from django.core.asgi import get_asgi_application
 
 
@@ -17,7 +20,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spotify_chat_app_project.settin
 
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
-})
+from chat.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
