26e02765db291d259474f85cdbe3a5064850d2ec
Annelies <118388665+Starianne@users.noreply.github.com>
2026-05-06 09:54:01 +0100

    removed / hope this works

--- spotify_chat_app_project/settings.py
+++ spotify_chat_app_project/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
-CSRF_TRUSTED_ORIGINS = ['https://musicchatapp-production.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://musicchatapp-production.up.railway.app']
 
 INSTALLED_APPS = [
     'daphne',
